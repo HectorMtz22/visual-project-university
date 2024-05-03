67702c427bab59c261277b0dc0076025326edc4d
Hector Martinez <hec.gre81@gmail.com>
2024-05-03 15:41:34 -0600

    Add code for problem 2

--- OPV_Equipo6/FormORD2.Designer.vb
+++ OPV_Equipo6/FormORD2.Designer.vb
@@ -27,10 +27,10 @@ Partial Class FormORD2
         Me.Label1 = New System.Windows.Forms.Label()
         Me.txtresultado = New System.Windows.Forms.TextBox()
         Me.txtvalor = New System.Windows.Forms.TextBox()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
         Me.btnconvertir = New System.Windows.Forms.Button()
-        Me.btnmenu = New System.Windows.Forms.Button()
+        Me.btn_menu = New System.Windows.Forms.Button()
+        Me.rb_cel_to_far = New System.Windows.Forms.RadioButton()
+        Me.rb_far_to_cel = New System.Windows.Forms.RadioButton()
         Me.SuspendLayout()
         '
         'Label3
@@ -57,7 +57,7 @@ Partial Class FormORD2
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(208, 35)
+        Me.Label1.Location = New System.Drawing.Point(146, 37)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(275, 29)
         Me.Label1.TabIndex = 3
@@ -65,73 +65,75 @@ Partial Class FormORD2
         '
         'txtresultado
         '
+        Me.txtresultado.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtresultado.Location = New System.Drawing.Point(283, 154)
-        Me.txtresultado.Multiline = True
         Me.txtresultado.Name = "txtresultado"
-        Me.txtresultado.Size = New System.Drawing.Size(280, 31)
-        Me.txtresultado.TabIndex = 7
+        Me.txtresultado.ReadOnly = True
+        Me.txtresultado.Size = New System.Drawing.Size(224, 32)
+        Me.txtresultado.TabIndex = 8
         '
         'txtvalor
         '
+        Me.txtvalor.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtvalor.Location = New System.Drawing.Point(57, 154)
-        Me.txtvalor.Multiline = True
         Me.txtvalor.Name = "txtvalor"
-        Me.txtvalor.Size = New System.Drawing.Size(164, 31)
-        Me.txtvalor.TabIndex = 8
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AccessibleDescription = ""
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox1.Location = New System.Drawing.Point(57, 238)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(201, 28)
-        Me.CheckBox1.TabIndex = 11
-        Me.CheckBox1.Text = "Celsius a Fahrenheit"
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'CheckBox2
-        '
-        Me.CheckBox2.AccessibleDescription = ""
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox2.Location = New System.Drawing.Point(57, 286)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(201, 28)
-        Me.CheckBox2.TabIndex = 12
-        Me.CheckBox2.Text = "Fahrenheit a Celsius"
-        Me.CheckBox2.UseVisualStyleBackColor = True
+        Me.txtvalor.Size = New System.Drawing.Size(164, 32)
+        Me.txtvalor.TabIndex = 7
         '
         'btnconvertir
         '
         Me.btnconvertir.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnconvertir.Location = New System.Drawing.Point(494, 235)
+        Me.btnconvertir.Location = New System.Drawing.Point(372, 221)
         Me.btnconvertir.Name = "btnconvertir"
         Me.btnconvertir.Size = New System.Drawing.Size(135, 31)
         Me.btnconvertir.TabIndex = 13
         Me.btnconvertir.Text = "Convertir"
         Me.btnconvertir.UseVisualStyleBackColor = True
         '
-        'btnmenu
+        'btn_menu
         '
-        Me.btnmenu.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnmenu.Location = New System.Drawing.Point(485, 355)
-        Me.btnmenu.Name = "btnmenu"
-        Me.btnmenu.Size = New System.Drawing.Size(144, 31)
-        Me.btnmenu.TabIndex = 14
-        Me.btnmenu.Text = "Menú principal"
-        Me.btnmenu.UseVisualStyleBackColor = True
+        Me.btn_menu.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_menu.Location = New System.Drawing.Point(363, 262)
+        Me.btn_menu.Name = "btn_menu"
+        Me.btn_menu.Size = New System.Drawing.Size(144, 31)
+        Me.btn_menu.TabIndex = 14
+        Me.btn_menu.Text = "Menú principal"
+        Me.btn_menu.UseVisualStyleBackColor = True
+        '
+        'rb_cel_to_far
+        '
+        Me.rb_cel_to_far.AutoSize = True
+        Me.rb_cel_to_far.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb_cel_to_far.Location = New System.Drawing.Point(57, 226)
+        Me.rb_cel_to_far.Name = "rb_cel_to_far"
+        Me.rb_cel_to_far.Size = New System.Drawing.Size(207, 26)
+        Me.rb_cel_to_far.TabIndex = 15
+        Me.rb_cel_to_far.TabStop = True
+        Me.rb_cel_to_far.Text = "Celsius -> Fahrenheit"
+        Me.rb_cel_to_far.UseVisualStyleBackColor = True
+        '
+        'rb_far_to_cel
+        '
+        Me.rb_far_to_cel.AutoCheck = False
+        Me.rb_far_to_cel.AutoSize = True
+        Me.rb_far_to_cel.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rb_far_to_cel.Location = New System.Drawing.Point(57, 262)
+        Me.rb_far_to_cel.Name = "rb_far_to_cel"
+        Me.rb_far_to_cel.Size = New System.Drawing.Size(207, 26)
+        Me.rb_far_to_cel.TabIndex = 16
+        Me.rb_far_to_cel.TabStop = True
+        Me.rb_far_to_cel.Text = "Fahrenheit -> Celsius"
+        Me.rb_far_to_cel.UseVisualStyleBackColor = True
         '
         'FormORD2
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(671, 450)
-        Me.Controls.Add(Me.btnmenu)
+        Me.ClientSize = New System.Drawing.Size(577, 317)
+        Me.Controls.Add(Me.rb_far_to_cel)
+        Me.Controls.Add(Me.rb_cel_to_far)
+        Me.Controls.Add(Me.btn_menu)
         Me.Controls.Add(Me.btnconvertir)
-        Me.Controls.Add(Me.CheckBox2)
-        Me.Controls.Add(Me.CheckBox1)
         Me.Controls.Add(Me.txtvalor)
         Me.Controls.Add(Me.txtresultado)
         Me.Controls.Add(Me.Label3)
@@ -149,8 +151,8 @@ Partial Class FormORD2
     Friend WithEvents Label1 As Label
     Friend WithEvents txtresultado As TextBox
     Friend WithEvents txtvalor As TextBox
-    Friend WithEvents CheckBox1 As CheckBox
-    Friend WithEvents CheckBox2 As CheckBox
     Friend WithEvents btnconvertir As Button
-    Friend WithEvents btnmenu As Button
+    Friend WithEvents btn_menu As Button
+    Friend WithEvents rb_cel_to_far As RadioButton
+    Friend WithEvents rb_far_to_cel As RadioButton
 End Class
--- OPV_Equipo6/FormORD2.vb
+++ OPV_Equipo6/FormORD2.vb
@@ -1,3 +1,38 @@
 ﻿Public Class FormORD2
+    Private Sub btn_menu_Click(sender As Object, e As EventArgs) Handles btn_menu.Click
+        FormMenu.Show()
+        Me.Hide()
+    End Sub
 
+    Function CelsiusAFahrenheit(celsius As Double) As Double
+        Return (celsius * 9 / 5) + 32
+    End Function
+
+    Function FahrenheitACelsius(fahrenheit As Double) As Double
+        Return (fahrenheit - 32) * 5 / 9
+    End Function
+
+    Private Sub BtnConvertir_Click(sender As Object, e As EventArgs) Handles btnconvertir.Click
+        Dim valorEntrada As Double
+        Dim resultado As Double
+
+        If Double.TryParse(txtvalor.Text, valorEntrada) Then
+            If rb_cel_to_far.Checked Then
+                resultado = CelsiusAFahrenheit(valorEntrada)
+            ElseIf rb_far_to_cel.Checked Then
+                resultado = FahrenheitACelsius(valorEntrada)
+            Else
+                MessageBox.Show("Sleccione una opción de conversión.")
+                Return
+            End If
+
+            txtresultado.Text = resultado.ToString("N2")
+        Else
+            MessageBox.Show("Ingrese un valor numérico válido.")
+        End If
+    End Sub
+
+    Private Sub FormORD2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rb_cel_to_far.Checked = True
+    End Sub
 End Class
