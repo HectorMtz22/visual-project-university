17428c2e7dcf5caf07bda214e3a80f8d6055790b
Hector Martinez <hec.gre81@gmail.com>
2024-05-08 16:53:56 -0600

    Add menu redirection

--- OPV_Equipo6/FormPIA3.vb
+++ OPV_Equipo6/FormPIA3.vb
@@ -1,3 +1,14 @@
 ﻿Public Class FormPIA3
+    Private Sub btn_agregar_Click(sender As Object, e As EventArgs) Handles btn_agregar.Click
 
+    End Sub
+
+    Private Sub btn_limpiar_Click(sender As Object, e As EventArgs) Handles btn_limpiar.Click
+
+    End Sub
+
+    Private Sub btn_menu_Click(sender As Object, e As EventArgs) Handles btn_menu.Click
+        FormMenu.Show()
+        Me.Hide()
+    End Sub
 End Class
