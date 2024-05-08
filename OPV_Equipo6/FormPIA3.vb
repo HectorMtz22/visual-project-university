2b1f8fd8c90196bed936050f26ed9c0dc79ca22f
Hector Martinez <hec.gre81@gmail.com>
2024-05-08 16:56:08 -0600

    Add function for clean inputs

--- OPV_Equipo6/FormPIA3.vb
+++ OPV_Equipo6/FormPIA3.vb
@@ -4,7 +4,10 @@
     End Sub
 
     Private Sub btn_limpiar_Click(sender As Object, e As EventArgs) Handles btn_limpiar.Click
-
+        dtp_fecha.Value = DateTime.Today
+        dtp_hora.Value = DateTime.Now
+        txt_titulo.Clear()
+        txt_descripcion.Clear()
     End Sub
 
     Private Sub btn_menu_Click(sender As Object, e As EventArgs) Handles btn_menu.Click
